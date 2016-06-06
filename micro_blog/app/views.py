c9116229cfdbab5b70876c6bd4cac8b212f47025
Alien <zjuguxi@gmail.com>
2016-06-06 18:08:35 +0800

    template

--- micro_blog/app/views.py
+++ micro_blog/app/views.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
-
+from flask import render_template
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'nickname': 'Miguel'} # 用户名
+    return render_template('index.html', title = 'Home', user = user)
