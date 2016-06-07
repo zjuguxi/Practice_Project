de9c414cc9f1bfff13c5d794c2509acdd5ef655d
Alien <zjuguxi@gmail.com>
2016-06-07 15:32:45 +0800

    forms

--- micro_blog/app/views.py
+++ micro_blog/app/views.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,8 @@ def index():
             }
     ]
     return render_template('index.html', title = 'Home', user = user, posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign In', form = form)
---
+++ micro_blog/config.py
@@ -0,0 +1,3 @@
+CSRF_ENABLED = True # 激活 跨站点请求伪造 保护
+
+SECRET_KEY = 'shi-yan-lou' # 仅当 CSRF 激活时需要，用来建立加密令牌以验证一个表单
