48432bee112923db517f94736f39a0c22bb8881d
Alien <zjuguxi@gmail.com>
2016-06-13 14:18:37 +0800

    blog init

---
+++ blog/app/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import Form
+from wtforms import TexrField, BooleanField, PasswordField
+from wtforms.validators import Required
+
---
+++ blog/app/views.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+from flask import render_template, flash, redirect
+from .forms import LoginForm
+from app import app
