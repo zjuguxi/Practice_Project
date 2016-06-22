89ec3254edab6bf98f00176f4c7c33a74d54692e
Alien <zjuguxi@gmail.com>
2016-06-22 16:46:58 +0800

    add

--- blog/run.py
+++ blog/run.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from views import  my_view
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
-import views
 
 app = Flask(__name__)
 manager = Manager(app)
 boostrap = Bootstrap(app)
 moment = Moment(app)
 
+
 if __name__ == '__main__':
-    manager.run(debug = True)
+    manager.run()
---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from datetime import datetime
+from run import app
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    return render_template('index.html', current_time = datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
