11609fb80cc49a7ae9b68432c2fc380253cdd36a
Alien <zjuguxi@gmail.com>
2016-06-22 16:47:26 +0800

    add

--- micro_blog/app/__init__.py
+++ micro_blog/app/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
