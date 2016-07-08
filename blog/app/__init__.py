7db7211516a6384c4d555b1f855e3d5e44eb15c3
Alien <zjuguxi@gmail.com>
2016-07-08 16:36:12 +0800

    add

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -12,11 +12,11 @@ def create_app():
     app = Flask(__name__)
     moment.init_app(app)
     bootstrap.init_app(app)
+    db = SQLAlchemy(app)
     app.secret_key = 'SECRET_KEY'
     db.init_app(app)
-    db = SQLAlchemy(app)
-    with app.app_context():
-           db.create_all()
+    #with app.app_context():
+     #      db.create_all()
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- blog/run.py
+++ blog/run.py
@@ -1,11 +1,14 @@
 # -*- coding:utf-8 -*-
 from app import create_app, db
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manage
+from flask_script import Manager, Shell
 
 app = create_app()
 manager = Manager(app)
 
+def make_shell_context():
+    return dict(app = app, db = db)
+manager.add_command('shell', Shell(make_context = make_shell_context))
 
 if  __name__ == '__main__':
     manager.run()
