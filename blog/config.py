e69a16b941cebf3b4f58e19149cdb5a931343d39
Alien <zjuguxi@gmail.com>
2016-07-13 14:10:31 +0800

    email

--- blog/config.py
+++ blog/config.py
@@ -5,16 +5,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    '''
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'zjuguxi@gmail.com'
+    MAIL_PASSWORD = 'guxi19880402'
+    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <zjuguxi@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-    '''
 
     @staticmethod
     def init_app(app):
