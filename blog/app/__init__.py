de2c7fbf442b9e58822c4eaee027f366ee2a7283
Alien <zjuguxi@gmail.com>
2016-07-13 12:59:41 +0800

    db create

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
+from config import config
 
 
 moment = Moment ()
@@ -10,13 +11,14 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config['default'])
+    config['default'].init_app(app)
     moment.init_app(app)
     bootstrap.init_app(app)
     db = SQLAlchemy(app)
     app.secret_key = 'SECRET_KEY'
     db.init_app(app)
-    #with app.app_context():
-     #      db.create_all()
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- blog/run.py
+++ blog/run.py
@@ -6,12 +6,6 @@ from flask_script import Manager, Shell
 
 app = create_app()
 manager = Manager(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 def make_shell_context():
     return dict(app = app, db = db)
