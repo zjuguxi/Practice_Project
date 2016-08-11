65f6018f663e406b33a4ddd18db294a0ce25cb63
Alien <zjuguxi@gmail.com>
2016-08-11 18:28:54 +0800

    add

--- dice_roller/dnd_dice.py
+++ dice_roller/dnd_dice.py
@@ -10,10 +10,10 @@ roll = input('> ')
 # 用正则表达式解析用户命令
 match = re.search(r'(\d+)([Dd])(\d+)', roll)
 
-# 判断输入语句是否合法——『ndn』模式
+# 判断输入语句是否合法——『ndn』格式
 if match:
     # 第一个数字，乘数，str
-    m = match.group(1)
+    m = int(match.group(1))
     # 第二个数字，骰子的面数，int(str)
     n = int(match.group(3))
 else:
@@ -23,8 +23,13 @@ else:
 # 定义n面骰
 def d(n):
     result = randint(1, n)
-    print(result)
+
+# 骰子的每个结果都放入一个list，初始为空
+dice = []
 
 # 重复投掷
-for i in m:
+for i in range(1, m+1):
    d(n)
+   dice.append(result)
+
+print(sum(dice))
