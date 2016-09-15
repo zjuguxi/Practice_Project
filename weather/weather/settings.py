1c52dd5ea5811c6b1e30f01b636db8f08687a58f
Alien <zjuguxi@gmail.com>
2016-09-15 17:26:46 +0800

    scrap

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -17,7 +17,17 @@ NEWSPIDER_MODULE = 'weather.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'weather (+http://www.yourdomain.com)'
+USER_AGENT = 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 
+DEFAULT_REQUEST_HEADERS = {
+    'Referer': 'http://www.weibo.com'
+}
+
+ITEM_PIPELINES = {
+    'weather.pipelines.WeatherPipeline': 1
+}
+
+DOWNLOAD_DELAY = 0.5
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
