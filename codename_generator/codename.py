7e5acbb6e104a966f5a150b1f07e6c3723484c66
Alien <zjuguxi@gmail.com>
2016-09-26 15:22:37 +0800

    fail

--- codename_generator/codename.py
+++ codename_generator/codename.py
@@ -1,9 +1,6 @@
-import pypdftk
-
-datas = {
-    'WORD1': 'Julien',
-    'WORD2': 'revolunet',
-    'WORD3': 42
-}
-generated_pdf = pypdftk.fill_form('./template.pdf', datas)
-out_pdf = pypdftk.concat(['./new.pdf', generated_pdf])
+from fdfgen import forge_fdf
+fields = [('name','John Smith'),('telephone','555-1234')]
+fdf = forge_fdf("",fields,[],[],[])
+fdf_file = open("data.fdf","w")
+fdf_file.write(fdf)
+fdf_file.close()
