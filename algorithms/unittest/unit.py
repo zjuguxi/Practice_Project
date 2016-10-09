ff9c8a286460d77ead519b8d69403585918250c3
Alien <zjuguxi@gmail.com>
2016-10-09 18:40:38 +0800

    unit test

---
+++ algorithms/unittest/unit.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestSort(unittest.TestCase):
+    def merge_sort(self):
+        self.assertEqual(merge_list, test_list)
+    def heap_sort(self):
+        self.assertEqual(heap_list, test_list)
+    def bubble_sort(self):
+        self.assertEqual(bubble_list, test_list)
+    def insert_sort(self):
+        self.assertEqual(insert_list, test_list)
+    def quick_sort(self):
+        self.assertEqual(quick_list, test_list)
+    def select_sort(self):
+        self.assertEqual(select_list, test_list)
+    def shell_sort(self):
+        self.assertEqual(shell_list, test_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
