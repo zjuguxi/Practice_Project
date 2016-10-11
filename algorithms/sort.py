65e2a16433613fa43c0bd785346fa8a763c97c28
Alien <zjuguxi@gmail.com>
2016-10-11 16:55:14 +0800

    debug

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -4,20 +4,28 @@ import sys
 
 sys.setrecursionlimit(999999)
 
-list_start = timeit.default_timer()
-origin_list = list(nprnd.randint(1000000, size=200))
-list_end = timeit.default_timer()
-list_time = round((list_end - list_start), 4)
-print('List time : %s s' % list_time)
-print('Length of the list: ', len(origin_list))
+def make_origin_list():
+    origin_list = list(nprnd.randint(1000000, size=200))
+    return origin_list
+
+origin_list = make_origin_list() 
 
 test_list = origin_list
 test_list_start = timeit.default_timer()
 test_list.sort()
 test_list_end = timeit.default_timer()
-test_list_time = round((list_end - list_start), 4)
+test_list_time = round((test_list_end - test_list_start), 4)
+
 print('Python sort time: %s s' % test_list_time)
 print('==============')
+
+list_start = timeit.default_timer()
+make_origin_list()
+list_end = timeit.default_timer()
+list_time = round((list_end - list_start), 4)
+print('List time : %s s' % list_time)
+print('Length of the list: ', len(origin_list))
+
 
 bubble_list = origin_list
 selection_list = origin_list
@@ -37,26 +45,25 @@ class Sort(object):
 
 class Bubble_sort(Sort): ############## Bubble Sort
     @classmethod
-    def sort(cls):
+    def sort(cls, list_to_sort):
         global bubble_elapse_time
-        n = len(bubble_list)
+        n = len(list_to_sort)
         bubble_start_time = timeit.default_timer()
 
         for i in range(n):
             for j in range(1, n - i):
-                if bubble_list[j - 1] > bubble_list[j]:
-                    bubble_list[j - 1], bubble_list[j] = bubble_list[j], bubble_list[j - 1]
-        # return bubble_list
+                if list_to_sort[j - 1] > list_to_sort[j]:
+                    list_to_sort[j - 1], list_to_sort[j] = list_to_sort[j], list_to_sort[j - 1]
+        return list_to_sort
 
         bubble_end_time = timeit.default_timer()
-        bubble_elapse_time = round((bubble_end_time - bubble_start_time), 3)
-        return bubble_elapse_time
+        bubble_elapse_time = round((bubble_end_time - bubble_start_time), 4)
+        #return bubble_elapse_time
         print('Bubble Sort Time: {}s'.format(bubble_elapse_time))
 
     @classmethod
     def get_time(cls):
         return bubble_elapse_time
-
 
 class Selection_sort(Sort): ############## Selection Sort
     @classmethod
--- algorithms/sort_test.py
+++ algorithms/sort_test.py
@@ -1,21 +1,27 @@
 import sys
 import unittest
-from sort import Sort
+from sort import Bubble_sort
 import numpy.random as nprnd
 
-sys.setrecursionlimit(999999)
-origin_list = list(nprnd.randint(1000000, size=200))
-print('Length of the list: ', len(origin_list))
+# sys.setrecursionlimit(999999)
+# origin_list = list(nprnd.randint(1000000, size=200))
+# print('Length of the list: ', len(origin_list))
 
-test_list = origin_list
-test_list.sort()
+# test_list = origin_list # 用Python内置函数sort()对test_list排序
+# test_list.sort()
 
-Bubble_sort.sort()
+# bubble_list = origin_list # 用冒泡算法对bubble_list排序
+# Bubble_sort.sort()
 
 class TestSort(unittest.TestCase):
 
+    def setUp(self):
+        self.origin_list = list(nprnd.randint(1000000, size=200))
+        bubble_list = self.origin_list
+        test_list = self.origin_list.sort()
+
     def test_bubble_sort(self):
-        self.assertEqual(Bubble_sort.sort(), test_list)
+        self.assertEqual(Bubble_sort.sort(self.origin_list), self.origin_list.sort())
 
 if __name__ == '__main__':
     unittest.main()
