621dd23e1050cf7391dea5ca94a3a95aea9170b3
Alien <zjuguxi@gmail.com>
2016-10-11 14:10:58 +0800

    select

--- algorithms/test.py
+++ algorithms/test.py
@@ -22,7 +22,8 @@ print('==============')
 bubble_list = origin_list
 selection_list = origin_list
 
-
+bubble_elapse_time = 0
+selection_elapse_time = 0
 class Sort(object):
     def __init__(self):
         pass
@@ -34,12 +35,11 @@ class Sort(object):
         pass
 
 
-class Bubble_sort(Sort):
+class Bubble_sort(Sort): ############## Bubble Sort
     @classmethod
     def sort(cls):
         global bubble_elapse_time
         n = len(bubble_list)
-
         bubble_start_time = timeit.default_timer()
 
         for i in range(n):
@@ -58,23 +58,23 @@ class Bubble_sort(Sort):
         return bubble_elapse_time
 
 
-                # class Selection_sort(Sort):
-                #     def sort(self):
-                #         n = len(selection_list)
-                #         for i in range(0,n):
-                #             min = i
-                #             for j in range(i+1,n):
-                #                 if selection_list[j] < selection_list[min] :
-                #                     min = j
-                #             selection_list[min],selection_list[i] = selection_list[i],selection_list[min]
-                #         return selection_list
+class Selection_sort(Sort): ############## Selection Sort
+    @classmethod
+    def sort(cls):
+        global selection_elapse_time
+        n = len(selection_list)
+        selection_start_time = timeit.default_timer()
+        for i in range(0,n):
+            min = i
+            for j in range(i+1,n):
+                if selection_list[j] < selection_list[min] :
+                    min = j
+            selection_list[min],selection_list[i] = selection_list[i],selection_list[min]
+        #return selection_list
+        selection_end_time = timeit.default_timer()
+        selection_elapse_time = round((selection_end_time - selection_start_time), 3)
+        return selection_elapse_time
 
-                #     def get_time(self):
-                #         select_start_time = timeit.default_timer()
-                #         sort(selection_list)
-                #         select_end_time = timeit.default_timer()
-                #         select_elapse_time = select_end_time - select_start_time
-                #         print("Selection Sort:  %s s " % (round(select_elapse_time, 3)))
-
-                # bubble = Bubble_sort(bubble_list)
-                # selection = Selection_sort(selection_list)
+    @classmethod
+    def get_time(cls):
+        return selection_elapse_time
